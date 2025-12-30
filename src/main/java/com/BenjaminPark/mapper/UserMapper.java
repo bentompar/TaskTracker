ac9daad89659db92521b0125cdbf8ba3d9ed25a7
Benjamin Park <bentompar@gmail.com>
2025-12-30 18:29:04 +0100

    Add UserMapper to map between User entities and DTOs

---
+++ src/main/java/com/BenjaminPark/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.BenjaminPark.mapper;
+
+import com.BenjaminPark.domain.User;
+import com.BenjaminPark.dto.CreateUserDTO;
+import com.BenjaminPark.dto.UpdateUserDTO;
+import com.BenjaminPark.dto.UserResponse;
+
+public class UserMapper {
+
+    /**
+     * Creates User entity from CreateUserDTO.
+     * @param createUserDTO CreateUserDTO to convert into user entity.
+     */
+    public User fromCreateUserDTO(CreateUserDTO createUserDTO) {
+        return new User(createUserDTO.getUsername(), createUserDTO.getPassword());
+    }
+
+    /**
+     * Returns updated User entity from updateUserDTO.
+     * @param user User to be updated.
+     * @param updateUserDTO UpdateUserDTO to convert existing user entity.
+     */
+    public User applyUpdate(User user, UpdateUserDTO updateUserDTO) {
+        user.setUsername(updateUserDTO.getUsername());
+        return user;
+    }
+
+    /**
+     * Returns UserResponse from user entity.
+     * @param user User entity converted into UserResponse entity.
+     */
+    public UserResponse toUserResponse(User user) {
+        return new UserResponse(user.getUserId().toString(), user.getUsername());
+    }
+
+}
